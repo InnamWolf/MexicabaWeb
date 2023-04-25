923f98ecb72c29b67d07906716fa54890b9f8ad8
VMRR <m.ramirez@ignilytic.com>
2023-04-24 23:20:08 -0600

    Se crea footer

---
+++ view/components/footer.php
@@ -0,0 +1,57 @@
+<!--===============================================
+FOOTER
+=================================================-->
+<footer> 
+
+    <div class="footer__container">
+
+      <div class="footer__top">      
+        <div class="footer-top__item"><a href="#">Fuerza de venta</a></div>
+        <div class="footer-top__item"><a href="#">Aviso de privacidad</a></div>
+      </div>
+
+      <div class="footer__div"></div>
+
+      <!--===============================================
+      REDES SOCIALES
+      =================================================-->
+      <div class="socialMedia">        
+
+          <a href="https://www.facebook.com/MEBEXoficial" target="_blank">            
+            <i class="fa-brands fa-facebook"></i>
+          </a>
+
+          <a href="https://www.instagram.com/mexicanadebecas/?hl=es" target="_blank">            
+            <i class="fa-brands fa-instagram"></i>
+          </a>
+
+          <a href="https://www.tiktok.com/@mexicana.de.becas?is_from_webapp=1&sender_device=pc" target="_blank">          
+            <i class="fa-brands fa-tiktok"></i>
+          </a>
+
+          <a href="https://www.youtube.com/user/mebexvideos" target="_blank">            
+            <i class="fa-brands fa-youtube"></i>
+          </a>
+
+          <a href="https://twitter.com/MexicanadeBecas" target="_blank">            
+            <i class="fa-brands fa-twitter"></i>
+          </a>
+
+          <a href="https://www.linkedin.com/company/mexicana-de-becas-excelencia-a-c-/" target="_blank">            
+            <i class="fa-brands fa-linkedin"></i>
+          </a>        
+        
+
+      </div>
+
+      <div class="footer__direction">
+        <p>Fideicomiso especializado para la educación operado por Santander (2003285)</p>
+        <p>Oficina de Atención a clientes: <br> Av. San Jerónimo No. 458, Col. Jardines del Pedregal, Álvaro Obregón, C.P.
+          01900, CDMX.
+          <br>Tels: (55) 5511 7377 – (55) 5511 8246 / 01 800 2322 700
+        </p>
+      </div>
+
+    </div>      
+
+</footer>
--- view/template.php
+++ view/template.php
@@ -28,8 +28,8 @@
 
 
   <!--===============================================
-lib js
-=================================================-->
+  lib js
+  =================================================-->
   <!-- JavaScript Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
   <!-- Jquery -->
