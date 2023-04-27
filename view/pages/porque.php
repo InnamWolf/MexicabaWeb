664ebc9ec26ef6b8ef0851c124a21c0c8286e5ac
VMRR <m.ramirez@ignilytic.com>
2023-04-26 23:43:09 -0600

    Se crea seccion porque

---
+++ view/pages/porque.php
@@ -0,0 +1,82 @@
+<section class="porque">
+	<!--===============================================
+	TITULO PORQUE
+	=================================================-->
+	<div class="tipoPorque">
+		<h3 class="titlePorque">
+			¿Por qué hacer un ahorro anticipado?
+		</h3>
+	</div>
+
+	<div class="info__existente"></div>
+
+	<div class="contenedor">
+
+		<div id="icono1" data-aos="anim1" data-aos-offset="500" data-aos-duration="new-transition"
+			data-aos-easing="ease-in-out-back"></div>
+
+		<div id="icono2" data-aos="anim2" data-aos-offset="400" data-aos-duration="new-transition"
+			data-aos-easing="ease-in-out-back"></div>
+
+		<div id="icono3" data-aos="anim3" data-aos-offset="500" data-aos-duration="new-transition"
+			data-aos-easing="ease-in-out-back"></div>
+
+		<div id="icono4" data-aos="anim4" data-aos-offset="400" data-aos-duration="new-transition"
+			data-aos-easing="ease-in-out-back"></div>
+
+		<div id="icono5" data-aos="anim5" data-aos-offset="500" data-aos-duration="new-transition"
+			data-aos-easing="ease-in-out-back"></div>
+
+		<div class="slider slider-for" data-aos="anim6" data-aos-offset="500" data-aos-duration="new-transition"
+			data-aos-easing="ease-in-out">
+
+			<div>
+				<div class="title__contenedor">Inflación educativa</div>
+				<div class="imgUno imgContenedor"></div>
+				<div class="txt__contenedor">
+					El costo de las Universidades privadas crece año con año. Por cada peso que en 2004 hubieras pagado de
+					colegiatura en una universidad privada,
+					hoy en promedio pagarías $2,298.
+				</div>
+			</div>
+
+			<div>
+				<div class="title__contenedor">Alto costo de universidades privadas</div>
+				<div class="imgDos imgContenedor"></div>
+				<div class="txt__contenedor">
+					El costo actual promedio de Universidades de costo elite como el Tec de Monterrey, Anáhuac e Ibero es de
+					$1,1575,000. En 18 años, se calcula que el costo promedio será de aproximadamente $2,786,000.
+				</div>
+			</div>
+			<div>
+				<div class="title__contenedor">Limitado acceso a universidades públicas</div>
+				<div class="imgTres imgContenedor"></div>
+				<div class="txt__contenedor">
+					La educación pública en nuestro país tiene mucha demanda y pocos lugares para ofertar. Solo 1 de cada 10
+					aspirantes consigue un lugar en la UNAM. De cada 100 mil aspirantes al IPN solo se quedan 23 mil.
+				</div>
+			</div>
+		</div>
+
+		<div class="slider slider-nav" data-aos="fade-right" data-aos-offset="500" data-aos-duration="new-transition"
+			data-aos-easing="ease-in-out">
+			<div>
+				<h3 id="option1">Inflación <br> educativa</h3>
+			</div>
+			<div>
+				<h3 id="option2">Altos costos</h3>
+			</div>
+			<div>
+				<h3 id="option3">Limitado<br> acceso</h3>
+			</div>
+		</div>
+	</div>
+
+	<!--===============================================
+	Boton cotizar
+	=================================================-->
+	<div class="btnCotizar">
+		<a href="#">¡Calcula cuánto necesitas ahorrar!</a>
+	</div>
+
+</section>
