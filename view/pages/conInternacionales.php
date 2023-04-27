74ea601f0c1605bcfbd0f784975b43cf7d3087a3
VMRR <m.ramirez@ignilytic.com>
2023-04-27 01:01:11 -0600

    Se crea seccion conInternacionales

---
+++ view/pages/conInternacionales.php
@@ -0,0 +1,295 @@
+<section class="conInternacionales">
+  <h1 class="conInternacionales__title">Convenios Universitarios</h1>
+
+  <div class="conInternacionales__subtitle conInternacionales__fondoOrange">
+    <div class="conInternacionales__itemFondo "><a  class="conInternacionales__convenio__inactivo" href="conNacionales">Nacionales</a></div>
+    <div class="conInternacionales__itemFondo"><a href="conInternacionales">Internacionales</a></div>
+  </div> 
+  
+  <p class="conInternacionales__txt">¿Qué significan los símbolos que encontrarás en las escuelas?</p>
+
+  <div class="conInternacionales__subtitle">Nivel:</div>
+  <div class="conInternacionales__simbologia__nivel">
+
+    <div class="conInternacionales__simbologia__item">  
+      <div class="conInternacionales__item__logo__mediaSuperior"></div>
+      <div class="conInternacionales__item__title">Nivel Medio Superior
+        <br>
+        <p class="conInternacionales__item__txt">(NMS) Bachillerato y/o Preparatoria</p>
+      </div>
+    </div>
+
+    <div class="conInternacionales__simbologia__item">  
+      <div class="conInternacionales__item__logo__superior"></div>
+      <div class="conInternacionales__item__title">Nivel Superior
+        <br>
+        <p class="conInternacionales__item__txt">(NS) Licenciaturas y/o Ingenierías</p>
+      </div>
+    </div>
+
+    <div class="conInternacionales__simbologia__item">  
+      <div class="conInternacionales__item__logo__posgrado"></div>
+      <div class="conInternacionales__item__title">Posgrado
+        <br>
+        <p class="conInternacionales__item__txt">(P) Maestrías y/o Doctorado</p>
+      </div>
+    </div>
+
+    <div class="conInternacionales__simbologia__item">  
+      <div class="conInternacionales__item__logo__educa"></div>
+      <div class="conInternacionales__item__title">Educación Continua
+        <br>
+        <p class="conInternacionales__item__txt">(EC) Diplomados, Cursos, Talleres, Especialidades y/o Centro de Idiomas</p>
+      </div>
+    </div>
+
+  </div>  
+
+
+  <div class="conInternacionales__subtitle">Aspirante:</div> 
+  <div class="conInternacionales__simbologia__aspirante">
+
+    <div class="conInternacionales__simbologia__item">  
+      <div class="conInternacionales__item__logo_beneficiario"></div>
+      <div class="conInternacionales__item__title">Beneficiarios</div>
+    </div>
+
+    <div class="conInternacionales__simbologia__item">  
+      <div class="conInternacionales__item__logo_suscriptor"></div>
+      <div class="conInternacionales__item__title">Suscriptores</div>
+    </div>
+
+    <div class="conInternacionales__simbologia__item">  
+      <div class="conInternacionales__item__logo_famDirecto"></div>
+      <div class="conInternacionales__item__title">Familiares Directos</div>
+    </div>
+
+  </div>  
+
+  <div class="conInternacionales__separacion"></div>
+
+  <div class="conInternacionales__subtitle conInternacionales__sombreado">Selecciona y explora los convenios de tu interés:</div>
+
+  <div class="conInternacionales__informacion">
+    <div class="conInternacionales__informacion__item shadow">
+      <img class="conInternacionales__informacion__imagen" src="view/src/img/convenios/enyd.svg" alt="Escuela de Negocios y Dirección" loading="lazy">
+      <div class="conInternacionales__informacion__txt" data-bs-toggle="modal" data-bs-target="#enyd"><i class="fa-solid fa-arrow-right"></i> Más </div>
+      <!-- Modal -->
+      <div class="modal fade" id="enyd" data-bs-backdrop="static">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header conInternacionales-header"> ESCUELA DE NEGOCIOS Y DIRECCIÓN
+              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body conInternacionales-body">
+              <img src="view/src/img/conNvlSuperior.svg" alt="Superior">
+              <img src="view/src/img/conPosgrado.svg" alt="Posgrado">
+              <img src="view/src/img/conEducacContinua.svg" alt="Continua">
+              <img src="view/src/img/alianzaBeneficiario.svg" alt="Beneficiario">
+              <img src="view/src/img/alianzaSus.svg" alt="Suscriptor">
+              <img src="view/src/img/alianzaFamDirectos.svg" alt="FamDirecto"><br>
+              <b>Inscripción:</b> 65% <br>
+              <b>Colegiatura:</b> 30% NS y posgrados <br>
+              Examen de admisión sin costo <br>
+              <b>Campus:</b> Madrid 
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn conInternacionales__btn-color-Gray btn-sm" data-bs-dismiss="modal">Salir</button>
+              <button type="button" class="btn conInternacionales__btn-color-Orange btn-sm" data-bs-dismiss="modal"><a href="https://www.escueladenegociosydireccion.com/" target="_blank">Sitio Escuela</a></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="conInternacionales__informacion__item shadow">
+      <img class="conInternacionales__informacion__imagen" src="view/src/img/convenios/University_of_Europe.svg" alt="University of Applied Sciences Europe" loading="lazy">
+      <div class="conInternacionales__informacion__txt" data-bs-toggle="modal" data-bs-target="#uoase"><i class="fa-solid fa-arrow-right"></i> Más </div>
+      <!-- Modal -->
+      <div class="modal fade" id="uoase" data-bs-backdrop="static">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header conInternacionales-header"> UNIVERSITY OF APPLIED SCIENCES EUROPE
+              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body conInternacionales-body">
+              <img src="view/src/img/conNvlSuperior.svg" alt="Superior">
+              <img src="view/src/img/conPosgrado.svg" alt="Posgrado">
+              <img src="view/src/img/alianzaBeneficiario.svg" alt="Beneficiario">
+              <img src="view/src/img/alianzaSus.svg" alt="Suscriptor">
+              <img src="view/src/img/alianzaFamDirectos.svg" alt="FamDirecto"><br>
+              <b>Inscripción:</b> 15% <br>
+              <b>Colegiatura:</b> 15% en matrícula  <br>
+              <b>Campus:</b> Iserlohn, Berlín, Hamburgo
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn conInternacionales__btn-color-Gray btn-sm" data-bs-dismiss="modal">Salir</button>
+              <button type="button" class="btn conInternacionales__btn-color-Orange btn-sm" data-bs-dismiss="modal"><a href="https://www.ue-germany.com/en/" target="_blank">Sitio Escuela</a></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="conInternacionales__informacion__item shadow">
+      <img class="conInternacionales__informacion__imagen" src="view/src/img/convenios/immune.svg" alt="Immune Tecnology institute" loading="lazy">
+      <div class="conInternacionales__informacion__txt" data-bs-toggle="modal" data-bs-target="#immune"><i class="fa-solid fa-arrow-right"></i> Más </div>
+      <!-- Modal -->
+      <div class="modal fade" id="immune" data-bs-backdrop="static">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header conInternacionales-header"> IMMUNE TECNOLOGY INSTITUTE
+              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body conInternacionales-body">
+              <img src="view/src/img/conNvlSuperior.svg" alt="Superior">
+              <img src="view/src/img/conPosgrado.svg" alt="Posgrado">
+              <img src="view/src/img/conEducacContinua.svg" alt="Continua">
+              <img src="view/src/img/alianzaBeneficiario.svg" alt="Beneficiario">
+              <img src="view/src/img/alianzaSus.svg" alt="Suscriptor">
+              <img src="view/src/img/alianzaFamDirectos.svg" alt="FamDirecto"><br>
+              <b>Inscripción:</b> N/A <br>
+              <b>Colegiatura:</b> 40% (NS, P y EC) <br> Examen de admisión sin costo <br>
+              <b>Campus:</b> Madrid, España 
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn conInternacionales__btn-color-Gray btn-sm" data-bs-dismiss="modal">Salir</button>
+              <button type="button" class="btn conInternacionales__btn-color-Orange btn-sm" data-bs-dismiss="modal"><a href="https://immune.institute/" target="_blank">Sitio Escuela</a></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="conInternacionales__informacion__item shadow">
+      <img class="conInternacionales__informacion__imagen" src="view/src/img/convenios/edulynks.svg" alt="EDULYNKS" loading="lazy">
+      <div class="conInternacionales__informacion__txt" data-bs-toggle="modal" data-bs-target="#edulynks"><i class="fa-solid fa-arrow-right"></i> Más </div>
+      <!-- Modal -->
+      <div class="modal fade" id="edulynks" data-bs-backdrop="static">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header conInternacionales-header"> EDULYNKS
+              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body conInternacionales-body">
+            <img src="view/src/img/conNvlMediSup.svg" alt="Medio">
+              <img src="view/src/img/conNvlSuperior.svg" alt="Superior">
+              <img src="view/src/img/conPosgrado.svg" alt="Posgrado">
+              <img src="view/src/img/conEducacContinua.svg" alt="Continua">
+              <img src="view/src/img/alianzaBeneficiario.svg" alt="Beneficiario">
+              <img src="view/src/img/alianzaSus.svg" alt="Suscriptor">
+              <img src="view/src/img/alianzaFamDirectos.svg" alt="FamDirecto"><br>
+              <b>Campus:</b> Canadá, Australía, EUA, España, entre otros
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn conInternacionales__btn-color-Gray btn-sm" data-bs-dismiss="modal">Salir</button>
+              <button type="button" class="btn conInternacionales__btn-color-Orange btn-sm" data-bs-dismiss="modal"><a href="http://www.edulynks.com" target="_blank">Sitio Escuela</a></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="conInternacionales__informacion__item shadow">
+      <img class="conInternacionales__informacion__imagen" src="view/src/img/convenios/be_international.svg" alt="Be International" loading="lazy">
+      <div class="conInternacionales__informacion__txt" data-bs-toggle="modal" data-bs-target="#beInternational"><i class="fa-solid fa-arrow-right"></i> Más </div>
+      <!-- Modal -->
+      <div class="modal fade" id="beInternational" data-bs-backdrop="static">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header conInternacionales-header"> BE INTERNATIONAL
+              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body conInternacionales-body">
+              <img src="view/src/img/conNvlSuperior.svg" alt="Superior">
+              <img src="view/src/img/conEducacContinua.svg" alt="Continua">
+              <img src="view/src/img/alianzaBeneficiario.svg" alt="Beneficiario">
+              <img src="view/src/img/alianzaSus.svg" alt="Suscriptor">
+              <img src="view/src/img/alianzaFamDirectos.svg" alt="FamDirecto"><br>
+              <b>Campus:</b> EUA, España, Inglaterra, Alemania
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn conInternacionales__btn-color-Gray btn-sm" data-bs-dismiss="modal">Salir</button>
+              <button type="button" class="btn conInternacionales__btn-color-Orange btn-sm" data-bs-dismiss="modal"><a href="http://www.beinternationalbecas.org/" target="_blank">Sitio Escuela</a></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="conInternacionales__informacion__item shadow">
+      <img class="conInternacionales__informacion__imagen" src="view/src/img/convenios/gus.svg" alt="Global University Systems" loading="lazy">
+      <div class="conInternacionales__informacion__txt" data-bs-toggle="modal" data-bs-target="#gus"><i class="fa-solid fa-arrow-right"></i> Más </div>
+      <!-- Modal -->
+      <div class="modal fade" id="gus" data-bs-backdrop="static">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header conInternacionales-header"> GLOBAL UNIVERSITY SYSTEMS
+              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body conInternacionales-body">
+              <img src="view/src/img/conNvlSuperior.svg" alt="Superior">
+              <img src="view/src/img/conPosgrado.svg" alt="Posgrado">
+              <img src="view/src/img/conEducacContinua.svg" alt="Continua">
+              <img src="view/src/img/alianzaBeneficiario.svg" alt="Beneficiario">
+              <img src="view/src/img/alianzaSus.svg" alt="Suscriptor">
+              <img src="view/src/img/alianzaFamDirectos.svg" alt="FamDirecto"><br>
+              <b>Campus:</b> Alemania, Irlanda y Canadá
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn conInternacionales__btn-color-Gray btn-sm" data-bs-dismiss="modal">Salir</button>
+              <button type="button" class="btn conInternacionales__btn-color-Orange btn-sm" data-bs-dismiss="modal"><a href="http://www.globaluniversitysystems.com/" target="_blank">Sitio Escuela</a></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="conInternacionales__informacion__item shadow">
+      <img class="conInternacionales__informacion__imagen" src="view/src/img/convenios/inspire_now.svg" alt="Inspire Now" loading="lazy">
+      <div class="conInternacionales__informacion__txt" data-bs-toggle="modal" data-bs-target="#inspireNow"><i class="fa-solid fa-arrow-right"></i> Más </div>
+      <!-- Modal -->
+      <div class="modal fade" id="inspireNow" data-bs-backdrop="static">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <!-- Modal Header -->
+            <div class="modal-header conInternacionales-header"> INSPIRE NOW
+              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-body conInternacionales-body">
+            <img src="view/src/img/conNvlMediSup.svg" alt="Medio">
+              <img src="view/src/img/conNvlSuperior.svg" alt="Superior">
+              <img src="view/src/img/conPosgrado.svg" alt="Posgrado">
+              <img src="view/src/img/conEducacContinua.svg" alt="Continua">
+              <img src="view/src/img/alianzaBeneficiario.svg" alt="Beneficiario">
+              <img src="view/src/img/alianzaSus.svg" alt="Suscriptor">
+              <img src="view/src/img/alianzaFamDirectos.svg" alt="FamDirecto"><br>
+              <b>Inscripción:</b> N/A <br>
+              <b>Campus:</b> Nueva Zelanda, Indonesia y Vietnam
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn conInternacionales__btn-color-Gray btn-sm" data-bs-dismiss="modal">Salir</button>
+              <button type="button" class="btn conInternacionales__btn-color-Orange btn-sm" data-bs-dismiss="modal"><a href="https://www.up.education/" target="_blank">Sitio Escuela</a></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</section>
