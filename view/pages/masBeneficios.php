8bd817e7bbae2f4ca3f67c8d9721a2dbf6ed3c30
VMRR <m.ramirez@ignilytic.com>
2023-04-26 23:54:27 -0600

    Se crea seccion masBeneficios

---
+++ view/pages/masBeneficios.php
@@ -0,0 +1,86 @@
+<section class="masBeneficios">
+
+  <div class="masBeneficios__title">
+  <h3> Al contratar un plan de ahorro educativo, inmediatamente obtienes los siguientes 
+    <span class="colorBlue">beneficios</span> :</h3>
+  </div>
+
+  <div class="masBeneficios__wrapper">
+
+    <div class="masBeneficios__item">
+
+      <div class="masBeneficios__card shadow ">
+          <div class="card__title">Convenios con universidades</div>
+          <div class="card__img01"></div>
+      </div>
+
+      <div class="masBeneficios_textos">
+
+        <p>
+          Acceso a una amplia red de convenios con universidades nacionales y extranjeras.
+        </p>
+
+        <p>
+          Obtendrás descuentos en colegiaturas y/o inscripciones con las instituciones en convenio vigentes.
+        </p>
+
+      </div>
+
+      <a href="conNacionales" class="masBeneficios__boton masBeneficios__boton--blue">
+         Ver la red de convenios
+      </a>
+
+    </div>
+
+    <div class="masBeneficios__item">
+
+      <div class="masBeneficios__card shadow ">
+          <div class="card__title">Red de alianzas</div>
+          <div class="card__img02"></div>
+      </div>
+
+      <div class="masBeneficios_textos">
+
+        <p>
+          Podrás obtener descuentos especiales en los productos
+          servicios que ofrecen las empresas participantes.
+        </p>
+
+        <p>
+          Por ejemplo: Librerías, ópticas, laboratorios médicos, y más.
+        </p>
+
+      </div>
+
+      <a href="alianzas" class="masBeneficios__boton masBeneficios__boton--blue">
+        Ver la red de aliados
+      </a>
+
+    </div>
+
+    <div class="masBeneficios__item">
+
+      <div class="masBeneficios__card shadow ">
+          <div class="card__title">Orientación vocacional</div>
+          <div class="card__img03"></div>
+      </div>
+
+      <div class="masBeneficios_textos">
+
+        <p>
+          Llegado el momento de elegir carrera, el futuro estudiante podrá
+          acceder gratis a una prueba on-line de orientación vocacional.                    
+        </p>   
+        
+      </div>
+
+      <a href="https://www.mb.com.mx/mbvocacional-registro/" class="masBeneficios__boton masBeneficios__boton--blue vocacional">
+         Solicitar prueba de orientación vocacional
+      </a>
+
+    </div>    
+    
+  </div>
+
+
+</section>
