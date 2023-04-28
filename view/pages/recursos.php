87e14743f68dfcf5b8a43839d227bf6dcba6c11e
VMRR <m.ramirez@ignilytic.com>
2023-04-27 23:03:58 -0600

    Se crea seccion Recursos

---
+++ view/pages/recursos.php
@@ -0,0 +1,55 @@
+<section class="recursos">
+
+    <div class="recursos__title">
+    <h3>Recursos</h3>
+    </div>
+
+    <p class='recursos__parrafos'>
+      Si estás pensando en ahorrar e invertir para la educación de tus pequeños estos recursos te serán de gran utilidad
+    </p>
+
+    <div class="recursos__items">
+
+      <a class="recursos__item__cuadro
+                shadow"
+                href="https://blog.mb.com.mx/"
+      >
+          <div class="item_cuadro_title">
+            Blog
+          </div>
+          <img src="view/src/img/recursos/blog.png" alt="blog">
+          <p class='item__parrafos'>
+            Encuentra artículos y material descargable sobre ahorro educativo.
+            <br><br>  
+          </p>
+      </a>
+
+      <a class="recursos__item__cuadro
+                shadow"
+                href="#"
+      >
+          <div class="item_cuadro_title">
+            Calculadora
+          </div>
+          <img src="view/src/img/recursos/calculadora.png" alt="calculadora">
+          <p class='item__parrafos'>
+            Calcula cuánto costará la educación de tus pequeños en el futuro y cuánto necesitas ahorrar para cumplir el objetivo.
+          </p>
+      </a>
+
+      <a class="recursos__item__cuadro
+                shadow"
+                href="videos"
+      >
+          <div class="item_cuadro_title">
+            Videos
+          </div>
+          <img src="view/src/img/recursos/videos.png" alt="videos">
+          <p class='item__parrafos'>
+            Mira nuestros videos y colaboraciones para obtener más información sobre nuestros Planes de Ahorro Educativo.
+          </p>
+      </a>
+
+    </div>
+    
+</section>
