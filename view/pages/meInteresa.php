b18a85ecbf3a9a1779b56c1d5b7002d191673a8e
VMRR <m.ramirez@ignilytic.com>
2023-04-27 22:47:06 -0600

    Se agrega seccion meInteresa

---
+++ view/pages/meInteresa.php
@@ -0,0 +1,54 @@
+<div class="interesaForm">
+
+  <h1 class="interesa__title">¡Quiero conocer más sobre Mexicana de Becas!</h1>
+  <p class="txtTitle">
+  Si te interesa recibir más información sobre nuestros planes de ahorro educativo, hacer una proyección de ahorro y armar un plan personalizado, déjanos tus datos y en breve te contactamos.
+  </p>
+
+  <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+  <script>
+    hbspt.forms.create({
+      region: "na1",
+      portalId: "2167727",
+      formId: "e01550a2-810f-42a4-b5e7-e9af5f36429d"
+    });
+  </script>
+
+  <div class="contactoInteresa">
+
+    <p><b>Contáctanos</b></p>
+    <p>Horario de atención al cliente</p>
+    <p>Lunes a Viernes de 8:30 am a 17:30 pm</p>
+
+    <div class="contactoBotones">
+
+      <d1iv class="btnTel btnAcomodo">
+
+        <div class="btnInterno">
+        <i class="fa-solid fa-mobile-screen-button"></i>
+        <span><b>Llama al (55) 5511 73177</b></span>
+        </div>   
+
+      </d1iv>
+
+      <div class="btnWhats btnAcomodo">
+
+        <div class="btnInterno">
+          <i class="fa-brands fa-whatsapp"></i>
+            <span><b>Esribenos por WhatsApp <br> (55) 3699 0663 </b></span>
+        </div>
+        
+      </div>
+
+      <div class="btnMail btnAcomodo">
+
+        <div class="btnInterno">
+          <i class="fa-regular fa-envelope"></i>
+            <span><b>Mándanos un correo <a href="mailto:contacto@mb.com.mx">contacto@mb.com.mx</a></b></span>          
+        </div>
+
+      </div>
+
+    </div>    
+  </div>
+</div>
