12024d0eaa43184d610a16ba14562a230f14f27c
VMRR <m.ramirez@ignilytic.com>
2023-05-22 22:57:51 -0600

    Se agrega seccion FAQ

---
+++ view/pages/faq.php
@@ -0,0 +1,257 @@
+<section class="faq">
+  <!--===============================================
+  TITULO
+  =================================================-->
+  <div class="faq__title">Preguntas frecuentes</div>
+
+  <!--===============================================
+  PREGUNTAS
+  =================================================-->
+  <div class="faq__inside">
+
+    <!-- FAQ-01 -->
+
+    <a class="faq__question" data-bs-toggle="collapse" href="#faq1" role="button" aria-expanded="false" aria-controls="faq1">
+      <span>¿Quién puede contratar un plan de ahorro educativo en Mexicana de Becas?</span> <i class="fa-solid fa-chevron-down"></i>
+    </a>
+
+    <div class="collapse faq__resp" id="faq1">
+
+      <div class="card card-body response">
+        El contratante puede crear un plan de ahorro educativo para sus hijos, nietos y sobrinos. Incluso, en México la
+        figura de “padrino” también puede contratar en Mexicana de Becas. El niño o niña debe estar cursando 3º grado de
+        primaria para plan de ahorro educativo para preparatoria, y en el caso de desear contratar el plan de
+        universidad, el niño o niña debe estar cursando como máximo 4º grado de primaria.
+      </div>
+      
+    </div>
+
+    <!-- FAQ-02 -->
+
+    <a class="faq__question" data-bs-toggle="collapse" href="#faq2" role="button" aria-expanded="false" aria-controls="faq2">
+      <span>¿Cuánto es lo mínimo que puedo ahorrar?</span> <i class="fa-solid fa-chevron-down"></i>
+    </a>
+
+    <div class="collapse faq__resp" id="faq2">
+
+      <div class="card card-body response">
+      No existe un mínimo de ahorro pues se trata de planes de ahorro totalmente personalizados de acuerdo con tus finanzas. Armamos un plan de ahorro educativo
+      a tu medida en donde tú decides el monto y la periodicidad de tu ahorro objetivo, en función de tus posibilidades económicas actuales: pago único, anual, semestral,
+      trimestral o mensual.
+      </div>
+      
+    </div>
+
+    <!-- FAQ-03 -->
+
+    <a class="faq__question" data-bs-toggle="collapse" href="#faq3" role="button" aria-expanded="false" aria-controls="faq3">
+      <span> ¿Qué edad deben tener mis hijos para poder contratar un plan de ahorro educativo con Mexicana de Becas?</span> <i class="fa-solid fa-chevron-down"></i>
+    </a>
+
+    <div class="collapse faq__resp" id="faq3">
+
+      <div class="card card-body response">
+       Mientras más pronto comiences es mejor. Lo ideal es empezar cuando ellos estén recién nacidos o son pequeños (entre 0 y 5 años), ya que los gastos que implican
+        no son tan grandes como los que se van a presentar conforme vayan creciendo. Además, debes considerar que la inflación educativa incrementa año con año.
+        <br> <br>
+        Para poder contratar un plan para Preparatoria, el niño o niña debe estar cursando 2º grado de primaria, y en el caso de desear contratar el plan de universidad, el niño
+        o niña debe estar cursando como máximo 4º grado de primaria.
+      </div>
+      
+    </div>
+
+    <!-- FAQ-04 -->
+
+    <a class="faq__question" data-bs-toggle="collapse" href="#faq4" role="button" aria-expanded="false" aria-controls="faq4">
+      <span> ¿Qué pasa si mi hijo decide no estudiar?</span> <i class="fa-solid fa-chevron-down"></i>
+    </a>
+
+    <div class="collapse faq__resp" id="faq4">
+
+      <div class="card card-body response">
+      Si llegado el momento de elegir carrera, el beneficiario se encuentra dudoso sobre qué estudiar, pueden posponer su ingreso a la universidad un año; y mientras tanto,
+      su dinero invertido seguirá generando rendimientos durante ese periodo de gracia.
+      <br> <br>
+      Si a pesar de esto, tu hijo(a) decide no estudiar, se puede optar por la opción de nombrar a otro beneficiario; siempre y cuando, este nuevo beneficiario sea menor
+      que el beneficiario original.
+      <br> <br>
+      El peor escenario es cancelar el plan, ya que como tiene el beneficio fiscal, si no se ocupa finalmente para educación, existe una penalización sobre el rendimiento.
+
+      </div>
+      
+    </div>
+
+    <!-- FAQ-05 -->
+
+    <a class="faq__question" data-bs-toggle="collapse" href="#faq5" role="button" aria-expanded="false" aria-controls="faq5">
+      <span> ¿Qué pasa si mi hijo estudia en una universidad pública o en alguna universidad que no esté dentro de la red de convenios de Mexicana de Becas?</span> <i class="fa-solid fa-chevron-down"></i>
+    </a>
+
+    <div class="collapse faq__resp" id="faq5">
+
+      <div class="card card-body response">
+        El beneficiario puede estudiar en la universidad que desee, pública o privada, nacional o extranjera, siempre y cuando tenga validez oficial. Hay muchas familias
+        que ahorraron con nosotros pensando en que probablemente tendrían que cubrir colegiaturas costosas y que él o la joven se encuentran estudiando en la UNAM,
+        IPN, UAM u otra institución pública.
+        <br> <br>
+        La red de convenios con instituciones privadas de prestigio tiene el objetivo de acercar descuentos con los cuales se pueden ahorrar hasta un año de colegiatura,
+        pero hay absoluta libertad de estudiar en la universidad o preparatoria, y carrera que desee; siempre y cuando estos cuenten con reconocimiento de validez oficial
+        en México o incluso en el extranjero, y sean legalmente reconocidas.
+        <br> <br>
+        En Mexicana de Becas no vendemos escuelas. Aunque te presentamos algunas opciones para ayudarte a realizar una proyección de tu ahorro, tú y tu hijo(a) tienen
+        absoluta libertad de elegir la universidad o preparatoria de su preferencia; siempre y cuando tenga validez y reconocimiento oficial.
+
+      </div>
+      
+    </div>
+
+    <!-- FAQ-06 -->
+
+    <a class="faq__question" data-bs-toggle="collapse" href="#faq6" role="button" aria-expanded="false" aria-controls="faq6">
+      <span> ¿Cómo sé que Mexicana de Becas es una institución confiable?</span> <i class="fa-solid fa-chevron-down"></i>
+    </a>
+
+    <div class="collapse faq__resp" id="faq6">
+
+      <div class="card card-body response">
+        <ul>
+          <li>
+            Cuentas con el respaldo operativo de Grupo Financiero Santander México como Fiduciario del fideicomiso, ya que en el que se encuentran invertidos
+            los recursos de tu ahorro. Esta institución capta, custodia, administra e invierte tus aportaciones.
+          </li> 
+          <br>
+          <li>
+            Tus recursos se invierten a través de un Fideicomiso de inversión y de administración abierto e irrevocable.
+          </li> 
+          <br>
+          <li>
+            Registro ante PROFECO (Número 3660-2011, Expediente Número PFC.B.E.7/003505-2011).
+          </li> 
+          <br>
+          <li>
+            Grupo Financiero Santander México lleva tu registro de forma individual y alimenta el sistema de Mexicana de Becas para la individualización de los
+            contratos. También, a nualmente Grupo Financiero Santander México emite un estado de cuenta paraque conozcas el estatus de tu inversión.
+          </li> 
+          <br>
+          <li>
+            A la fecha, se han entregado más de $2,855 millones de pesos a más de 22,708 jóvenes para su educación*
+          </li> 
+          <br>
+          <li>
+            Tenemos 30 años de experiencia, más de 57,900 suscriptores históricos y un fondo acumulado de más de $3,000 millones de pesos* administrado por Banco Santander.
+          </li> 
+        </ul>
+        <br>
+        *Datos al corte del 31 de julio 2022
+
+      </div>
+      
+    </div>
+
+    <!-- FAQ-07 -->
+
+    <a class="faq__question" data-bs-toggle="collapse" href="#faq7" role="button" aria-expanded="false" aria-controls="faq7">
+      <span> ¿Qué es el gasto de administración?</span> <i class="fa-solid fa-chevron-down"></i>
+    </a>
+
+    <div class="collapse faq__resp" id="faq7">
+
+      <div class="card card-body response">
+        Este gasto además de ser el costo por la gestión de tus recursos a lo largo de la vida de tu plan de ahorro, te da acceso a múltiples beneficios adicionales durante el
+        tiempo en que tu inversión esté con nosotros. Se cobra únicamente durante las primeras a portaciones** de tu plan de ahorro.
+        <br> <br>
+        <span class="faq__txt--Small">** Pueden ser de 12 a 14 aportaciones mensuales o su equivalente de acuerdo con la periodicidad de aportación y tipo de Plan. </span>
+      </div>
+      
+    </div>
+
+    <!-- FAQ-08 -->
+
+    <a class="faq__question" data-bs-toggle="collapse" href="#faq8" role="button" aria-expanded="false" aria-controls="faq8">
+      <span> ¿Para qué sirve el seguro de pago saldado?</span> <i class="fa-solid fa-chevron-down"></i>
+    </a>
+
+    <div class="collapse faq__resp" id="faq8">
+
+      <div class="card card-body response">
+        Este seguro protege las aportaciones remanentes* de tu plan de ahorro, con la finalidad de asegurar la educación de tu beneficiario en el caso de que cualquiera
+        de los contratantes se viera limitado para continuar realizando sus aportaciones en caso de fallecimiento o invalidez total o permanente.
+        <br> <br>
+        <span class="faq__txt--Small">** Que faltan por cubrir. </span>
+      </div>
+      
+    </div>
+
+    <!-- FAQ-09 -->
+
+    <a class="faq__question" data-bs-toggle="collapse" href="#faq9" role="button" aria-expanded="false" aria-controls="faq9">
+      <span>  ¿Puedo volver a contratar un plan al finalizar uno?</span> <i class="fa-solid fa-chevron-down"></i>
+    </a>
+
+    <div class="collapse faq__resp" id="faq9">
+
+      <div class="card card-body response">
+        Si, siempre y cuando el beneficiario se encuentre cursando como máximo 4° año de primaria para Plan de universidad y 2° año primaria para Plan de preparatoria.
+      </div>
+      
+    </div>
+
+    <!-- FAQ-10 -->
+
+    <a class="faq__question" data-bs-toggle="collapse" href="#faq10" role="button" aria-expanded="false" aria-controls="faq10">
+      <span>  ¿Qué pasa si estoy atravesando por un momento económico difícil y no puedo continuar aportando a mi plan de ahorro contratado?</span> <i class="fa-solid fa-chevron-down"></i>
+    </a>
+
+    <div class="collapse faq__resp" id="faq10">
+
+      <div class="card card-body response">
+        En este caso, el contratante titular puede comunicarse con Mexicana de Becas para indicarnos que está atravesando un momento económico difícil y buscar alternativas
+        para sobrellevar esta situación, que le permitan continuar con el plan de ahorro y evitar llegar a la cancelación, que es lo menos recomendable.
+        <br><br>
+        Es importante señalar que dichas alternativas tienen que ver con la postergación de las aportaciones planeadas, por lo cual la estimación que se tenía en un inicio de
+        cuánto monto iba a generar en el futuro, definitivamente va a variar, ya que durante un periodo se estaría interrumpiendo el ahorro.
+      </div>
+      
+    </div>
+
+    <!-- FAQ-11 -->
+
+    <a class="faq__question" data-bs-toggle="collapse" href="#faq11" role="button" aria-expanded="false" aria-controls="faq11">
+      <span> ¿Qué pasa si cancelo?</span> <i class="fa-solid fa-chevron-down"></i>
+    </a>
+
+    <div class="collapse faq__resp" id="faq11">
+
+      <div class="card card-body response">
+        De manera general, el tipo de penalización va a depender del momento en el que se hace la cancelación, y estas serán sobre los rendimientos generados. Al existir
+        el beneficio fiscal y no cumplirse el objetivo final que es la educación del beneficiario, existirá una penalización sobre el rendimiento, la cual se expresa en el contrato.
+      </div>
+      
+    </div>
+
+    <!-- FAQ-12 -->
+
+    <a class="faq__question" data-bs-toggle="collapse" href="#faq12" role="button" aria-expanded="false" aria-controls="faq12">
+      <span> ¿Puedo incrementar la suma que contraté al principio?</span> <i class="fa-solid fa-chevron-down"></i>
+    </a>
+
+    <div class="collapse faq__resp" id="faq12">
+
+      <div class="card card-body response">
+        
+        Si el contrato se encuentra cerrado o durante el periodo de aportaciones y el beneficiario se encuentra en posibilidad económica para adquirir un nuevo contrato,
+        puede incrementar su inversión a través de la contratación de un nuevo contrato.
+        <br><br>
+        Una vez que haya concluido con sus aportaciones, pero el beneficiario se encuentra fuera del rango de edad para adquirir un nuevo contrato y no ha llegado el Año de
+        Rentabilidad*, puede habilitar la promoción de Ahorra un año más, con la cual puede seguir aportando directo a su capital durante 1 año adicional.el contrato.
+        <br><br>
+        <span class="faq__txt--Small">*Año de Rentabilidad: año en el cual inicia la fase de reembolso, esto de acuerdo con el contrato.</span>
+
+      </div>
+      
+    </div>
+
+  </div>
+
+  
+</section>
