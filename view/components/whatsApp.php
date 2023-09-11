412f53aa61f51bb148bcf3654075bf87c74e4b09
VMRR <m.ramirez@ignilytic.com>
2023-09-10 23:58:55 -0600

    Se actualiza numero whatsapp

--- view/components/whatsApp.php
+++ view/components/whatsApp.php
@@ -1,5 +1,6 @@
 <div class="whatsApp" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-content=" ¿Tienes dudas? ¡Escríbenos!" data-bs-placement="top">
-  <a href="https://api.whatsapp.com/send/?phone=525536990663&type=phone_number&app_absent=0">
+  <!-- <a href="https://api.whatsapp.com/send/?phone=525536990663&type=phone_number&app_absent=0"> -->
+  <a href="https://api.whatsapp.com/send?phone=5215536990663&text=Quiero%20m%C3%A1s%20informaci%C3%B3n%20sobre%20los%20planes%20de%20ahorro%20educativo%20%F0%9F%A4%91%F0%9F%91%A9%E2%80%8D%F0%9F%8E%93">
     <img class="boton" src="view/src/img/whats.svg" alt="whatsApp">
   </a>
 </div>
