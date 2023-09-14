0983b68134a2f40da1e00bfaec7fad5fb058f2dc
VMRR <m.ramirez@ignilytic.com>
2023-09-14 01:28:37 -0600

    Se agrego componente cookie

---
+++ view/components/cookies.php
@@ -0,0 +1,11 @@
+<!--===============================================
+Cookies 
+=================================================-->
+<section class="cookie" id="cookie">
+    <div class="ctext">Al hacer clic en “Aceptar todas las cookies”, usted acepta que las cookies se guarden en su dispositivo para mejorar la navegación del sitio, analizar el uso del mismo, y colaborar con nuestros estudios para marketing.</div>
+    <div class="cbtn">
+        <button class="boton" id="aceptar">Aceptar todas las cookies</button>
+    </div>
+</section>
+
+<div class="fondo-cookie" id="fondo-cookie"></div>
