b39d39e84d71d330b099b40a395fff6ee78efbd1
VMRR <m.ramirez@ignilytic.com>
2023-09-19 23:05:29 -0600

    Se actualiza seccion recursos para revista

--- view/pages/recursos.php
+++ view/pages/recursos.php
@@ -1,55 +1,85 @@
 <section class="recursos">
 
-    <div class="recursos__title">
-      <h3>Recursos</h3>
-    </div>
+  <div class="recursos__title">
+    <h3>Recursos</h3>
+  </div>
 
-    <p class='recursos__parrafos'>
-      Si estás pensando en ahorrar e invertir para la educación de tus pequeños estos recursos te serán de gran utilidad
-    </p>
+  <p class='recursos__parrafos'>
+    Si estás pensando en ahorrar e invertir para la educación de tus pequeños estos recursos te serán de gran utilidad
+  </p>
 
-    <div class="recursos__items">
+  <div class="recursos__items">
 
-      <a class="recursos__item__cuadro shadow" href="https://blog.mb.com.mx/">
-        <div class="item_cuadro_title recurso__title--orange">
-          Blog
-        </div>
-        <img src="view/src/img/recursos/blog.png" alt="blog">
-        <p class='item__parrafos'>
-          Encuentra artículos y material descargable sobre ahorro educativo.
-          <br><br>  
-        </p>
-        <div class="recurso__item--boton recurso__title--orange">
-          Visitar
-        </div>
-      </a>
+    <a class="recursos__item__cuadro shadow" href="https://blog.mb.com.mx/">
 
-      <a class="recursos__item__cuadro shadow" href="#">
+      <div class="item_cuadro_title recurso__title--orange">
+        Blog
+      </div>
+      <img src="view/src/img/recursos/blog.png" alt="blog">
+      <p class='item__parrafos'>
+        Encuentra artículos y material descargable sobre ahorro educativo.
+        <br><br>  
+      </p>
+      <div class="recurso__item--boton recurso__title--orange">
+        Visitar
+      </div>
+
+    </a>
+
+    <a class="recursos__item__cuadro shadow" href="#">
+
       <div class="item_cuadro_title recurso__title--blue">
         Calculadora
       </div>
+
       <img src="view/src/img/recursos/calculadora.png" alt="calculadora">
-        <p class='item__parrafos'>
-          Calcula cuánto costará la educación de tus pequeños en el futuro y cuánto necesitas ahorrar para cumplir el objetivo.
-        </p>
-        <div class="recurso__item--boton recurso__title--blue">
-          Calcular
-        </div>
-      </a>
 
-      <a class="recursos__item__cuadro shadow" href="videos">
-        <div class="item_cuadro_title recurso__title--red">
-          Videos
-        </div>
-        <img src="view/src/img/recursos/videos.png" alt="videos">
-        <p class='item__parrafos'>
-          Mira nuestros videos y colaboraciones para obtener más información sobre nuestros Planes de Ahorro Educativo.
-        </p>
-        <div class="recurso__item--boton recurso__title--red">
-          Ver
-        </div>
-      </a>
+      <p class='item__parrafos'>
+        Calcula cuánto costará la educación de tus pequeños en el futuro y cuánto necesitas ahorrar para cumplir el objetivo.
+      </p>
 
-    </div>
+      <div class="recurso__item--boton recurso__title--blue">
+        Calcular
+      </div>
+
+    </a>
+
+    <a class="recursos__item__cuadro shadow" href="videos">
+
+      <div class="item_cuadro_title recurso__title--red">
+        Videos
+      </div>
+
+      <img src="view/src/img/recursos/videos.png" alt="videos">
+
+      <p class='item__parrafos'>
+        Mira nuestros videos y colaboraciones para obtener más información sobre nuestros Planes de Ahorro Educativo.
+      </p>
+
+      <div class="recurso__item--boton recurso__title--red">
+        Ver
+      </div>
+
+    </a>
+
+    <a class="recursos__item__cuadro shadow" href="videos">
+
+      <div class="item_cuadro_title recurso__title--green">
+        Revista Futuro MB
+      </div>
+
+      <img src="view/src/img/recursos/videos.png" alt="videos">
+
+      <p class='item__parrafos'>
+        Consulta nuestra revista digital con artículos redactados por nuestros convenios y aliados sobre educación, finanzas y más.
+      </p>
+
+      <div class="recurso__item--boton recurso__title--green">
+        Leer
+      </div>
+      
+    </a>
+
+  </div>
     
 </section>
