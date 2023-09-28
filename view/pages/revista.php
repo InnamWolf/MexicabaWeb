7e913c5fdec8f513bc5147a0ce2c5249761b1b05
VMRR <m.ramirez@ignilytic.com>
2023-09-28 12:33:33 -0600

    Se actuliza link para revistas digitales

--- view/pages/revista.php
+++ view/pages/revista.php
@@ -11,17 +11,17 @@
     <div class="revOnline">
         <div class="item">
             <img src="view/src/img/recursos/22.png" alt="revita22">
-            <a href="#" target="_blank"">Leer</a>
+            <a href="https://www.mb.com.mx/home/RevistaFuturoMb-Historico/2023/Mayo-Agosto/" target="_blank"">Leer</a>
         </div>
 
         <div class="item">
-            <img src="view/src/img/recursos/21.png" alt="revita22">
-            <a href="#" target="_blank"">Leer</a>
+            <img src="view/src/img/recursos/21.png" alt="revita21">
+            <a href="https://www.mb.com.mx/home/RevistaFuturoMb-Historico/2023/Enero-Abril/" target="_blank"">Leer</a>
         </div>
 
         <div class="item">
-            <img src="view/src/img/recursos/20.png" alt="revita22">
-            <a href="#" target="_blank"">Leer</a>
+            <img src="view/src/img/recursos/20.png" alt="revita20">
+            <a href="https://www.mb.com.mx/home/RevistaFuturoMb-Historico/2022/Septiembre-Diciembre/" target="_blank"">Leer</a>
         </div>
     </div>
 
